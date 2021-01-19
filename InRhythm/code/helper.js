d4026853733e5b6c5152df24d0b6232e7347c3ce
Imran Sahil <imdshizzle@gmail.com>
2021-01-19 13:00:41 -0500

    Add files via upload
    first push with code and unit tests

---
+++ InRhythm/code/helper.js
@@ -0,0 +1,25 @@
+export default class Helper {
+    
+    wordLength = word => word.split('').length; //return the length of the word passed in the parameter.
+
+    longestWordInASentence = sentence => {
+        if(!sentence || !sentence.length) return undefined;
+        //we can also return 0 which will be a falsy value.
+        //if we return a string with a message e.g. 'error', the unit tests will fail.
+        const wordsArray = sentence.split(' '); 
+        //since in a sentence new words starts after a space we will split the sentence in chunks after any space occurs.
+        
+        let longestWord, maxLength = 0;
+        wordsArray.forEach(word => {
+            const length = this.wordLength(word);
+            if(length > maxLength){ 
+                //if the condition returns truthy we will replace the value for 'maxLength' & 'longestWord' objects.
+                longestWord = word;
+                maxLength = length;
+            }
+        })
+
+        return {maxLength, longestWord}
+    } 
+
+}
---
+++ InRhythm/test/longestWord.test.js
@@ -0,0 +1,39 @@
+import Helper from '../code/helper.js'
+const helper = new Helper(); //instantiate the Helper class so you can access the methods inside it.
+
+describe('Unit tests for longestWordInASentence method implementation', () => {
+
+    test('An empty sentence should return falsy', () => {
+        const result = helper.longestWordInASentence('');
+        expect(result).toBeFalsy();
+    })
+
+    test('An empty parameter should return falsy', () => {
+        const result = helper.longestWordInASentence();
+        expect(result).toBeFalsy();
+    })
+
+    test("One word sentence should return the word & it's length", () => {
+        const {maxLength, longestWord} = helper.longestWordInASentence('Hello');
+        // console.log(`Max length: ${maxLength}. Longest word: ${longestWord}`) //we can use console logs for debugging purpose.
+        expect(maxLength).toEqual(5);
+        expect(longestWord).toMatch('Hello');
+    })
+
+    test('Max length of the longest word in a full sentence', () => {
+        const {maxLength, longestWord} = helper.longestWordInASentence('The cow jumped over the moon.');
+        expect(maxLength).toEqual(6);
+        expect(longestWord).toMatch('jumped');
+    })
+
+    test('Passing a number instead of a string in the parameter should return falsy', () => {
+        const result = helper.longestWordInASentence(123);
+        expect(result).toBeFalsy();
+    })
+
+    test("Passing a number in string format should return the number and it's length", () => {
+        const {maxLength, longestWord} = helper.longestWordInASentence('123');
+        expect(maxLength).toEqual(3);
+        expect(longestWord).toMatch('123');
+    })
+})
